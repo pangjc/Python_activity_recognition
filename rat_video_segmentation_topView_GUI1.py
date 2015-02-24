b9603d505163d7e216542fcad1fd16e2deeaa2b3
unknown <PANGJ05@AMRL2349PB17B2K.amer.pfizer.com>
2015-02-24 13:45:13 -0500

    The empty video clip bug is fixed. Reason: fps = 0.00 for some unknown reasaon.
    Enhanced the visualization effect.
    
    Signed-off-by: unknown <PANGJ05@AMRL2349PB17B2K.amer.pfizer.com>

--- rat_video_segmentation_topView_GUI1.py
+++ rat_video_segmentation_topView_GUI1.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 from pylab import ginput
 import csv
 import os.path
+from cv2 import VideoCapture
 
 # A gui version for video segmentation 
-# This script is customized from 'rat_video_processing_GUI_v2_0.py' for Meera Modi's experiment setup
-# Works for the right and left cameras
+# Modification over version 0: use module blocks for rats over left and right 
 
 class Window(tk.Frame):
     
@@ -27,7 +27,57 @@ class Window(tk.Frame):
         loadVideoButton.place(x = 0, y = 0)
         quitButton.place(x = 80, y = 0)       
  
+    
+ 
     def client_load(self):
+        
+        def segmentation_frame(self,frame0,frame_base,maskRegion,kernel):
+                
+            frame = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
+            frame = cv2.GaussianBlur(frame,(7,7),0)  
+            diff = (frame-frame_base)*maskRegion
+            
+            ret,fgmask0 = cv2.threshold(diff,150,255,cv2.THRESH_BINARY_INV)
+            
+            fgmask0 = fgmask0*maskRegion
+            fgmask1 = cv2.morphologyEx(fgmask0,cv2.MORPH_OPEN,kernel)             
+            fgmask2 = np.zeros_like(fgmask1,np.uint8)
+            
+            contour0, hier0 = cv2.findContours(fgmask1,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+            maxAreaSize = 0
+            maxInd = 0
+            
+            # Get the largest component and fill the holes by redrawing it
+            if contour0:
+                for jj in range(0,len(contour0)):
+                    if cv2.contourArea(contour0[jj]) > maxAreaSize:
+                        maxAreaSize = cv2.contourArea(contour0[jj]) 
+                        maxInd = jj  
+                # Draw the largest component with holes filled                
+                cv2.drawContours(fgmask2,[contour0[maxInd]],0,255,-1)
+                # Get the orientation        
+                (junk1,junk2),(MA,ma),orientation= cv2.fitEllipse(contour0[maxInd])
+                # Get the centroid
+                M = cv2.moments(contour0[maxInd])
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+    
+            mouse = np.zeros_like(frame0)
+    
+            mouse[:,:,1] = fgmask2
+            mouse[:,:,2] = fgmask2
+          
+            if contour0:
+                mouse[cy-1:cy+1,cx-1:cx+1,0] = 0
+                mouse[cy-1:cy+1,cx-1:cx+1,1] = 0
+              
+            mouse_size = np.count_nonzero(fgmask2)  
+            features =  [cx, cy, mouse_size, orientation]
+            
+            return mouse,features
+        
+        # end of function definition
+        
         videoName = tkFileDialog.askopenfilename()
         (dirName,videoBaseName0) = os.path.split(videoName)
         (videoBaseName, videoExtension) = os.path.splitext(videoBaseName0)
@@ -39,15 +89,15 @@ class Window(tk.Frame):
         ###########################
         ###########################
         ###########################
-        # Select points to detrmine Y maze arms
+        # Select points to determine effective regions
         cap = cv2.VideoCapture(videoName)
         cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,200)
         ret,frame_input0 = cap.read()      
         frame_input = cv2.cvtColor(frame_input0,cv2.COLOR_BGR2GRAY)
         cap.release()
-        
+               
         inputFig = plt.figure()
-        plt.title('Select arm points')    
+        plt.title('Select points to define effective regions')    
         plt.imshow(frame_input, cmap = 'gray', interpolation = 'bicubic')
         
         # Specify output video
@@ -94,10 +144,6 @@ class Window(tk.Frame):
         
         YarmImageName =  dirName+"/"+videoBaseName+"_effect_region.tif" 
         cv2.imwrite(YarmImageName,mask_toSave)
-        
-        ###########################
-        ###########################
-        ###########################
         
         ###########################
         ###########################
@@ -153,74 +199,11 @@ class Window(tk.Frame):
         for ii in range(50,length-10):
             ret,frame0 = cap.read()
             
-            frame = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-            frame = cv2.GaussianBlur(frame,(7,7),0)  
-            diff = (frame-frame_base)*mask
-              
-            ret,fgmask0 = cv2.threshold(diff,150,255,cv2.THRESH_BINARY_INV)
+            mouseLeft, featuresLeft = segmentation_frame(self,frame0,frame_base,maskLeft,kernel)
+            mouseRight, featuresRight = segmentation_frame(self,frame0,frame_base,maskRight,kernel)
             
-            fgmask01 = fgmask0*maskLeft
-            fgmask02 = fgmask0*maskRight
-            
-            fgmask11 = cv2.morphologyEx(fgmask01,cv2.MORPH_OPEN,kernel)   
-            fgmask12 = cv2.morphologyEx(fgmask02,cv2.MORPH_OPEN,kernel)           
-            fgmask21 = np.zeros_like(fgmask11,np.uint8)
-            fgmask22 = np.zeros_like(fgmask12,np.uint8)
-            
-            # fgmask2 = img_as_ubyte(morphology.remove_small_objects(fgmask1>0, areaSize))
-            # Only keep the component with the largest area size
-            contour01, hier01 = cv2.findContours(fgmask11,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-            maxAreaSize1 = 0
-            maxInd1 = 0
-            
-            # Get the largest component and fill the holes by redrawing it
-            if contour01:
-                for jj in range(0,len(contour01)):
-                    if cv2.contourArea(contour01[jj]) > maxAreaSize1:
-                        maxAreaSize1 = cv2.contourArea(contour01[jj]) 
-                        maxInd1 = jj  
-                # Draw the largest component with holes filled                
-                cv2.drawContours(fgmask21,[contour01[maxInd1]],0,255,-1)
-                # Get the orientation        
-                (junk11,junk12),(MA1,ma1),orientation1 = cv2.fitEllipse(contour01[maxInd1])
-                # Get the centroid
-                M1 = cv2.moments(contour01[maxInd1])
-                cx1 = int(M1['m10']/M1['m00'])
-                cy1 = int(M1['m01']/M1['m00'])
-                     
-            # Only keep the component with the largest area size
-            contour02, hier02 = cv2.findContours(fgmask12,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-            maxAreaSize2 = 0
-            maxInd2 = 0
-            
-            # Get the largest component and fill the holes by redrawing it
-            if contour02:
-                for jj in range(0,len(contour02)):
-                    if cv2.contourArea(contour02[jj]) > maxAreaSize2:
-                        maxAreaSize2 = cv2.contourArea(contour02[jj]) 
-                        maxInd2 = jj  
-                # Draw the largest component with holes filled                
-                cv2.drawContours(fgmask22,[contour02[maxInd2]],0,255,-1)
-                # Get the orientation        
-                (junk21,junk22),(MA2,ma2),orientation2 = cv2.fitEllipse(contour02[maxInd2])
-                # Get the centroid
-                M2 = cv2.moments(contour02[maxInd2])
-                cx2 = int(M2['m10']/M2['m00'])
-                cy2 = int(M2['m01']/M2['m00'])
-                
-            
-            mouse = np.zeros_like(frame0)
-
-            mouse[:,:,1] = cv2.add(fgmask21,fgmask22)
-            mouse[:,:,2] = cv2.add(fgmask21,fgmask22)
-        
-            if contour01:
-                mouse[cy1-1:cy1+1,cx1-1:cx1+1,0] = 0
-                mouse[cy1-1:cy1+1,cx1-1:cx1+1,1] = 0
-            if contour01:
-                mouse[cy2-1:cy2+1,cx2-1:cx2+1,0] = 0
-                mouse[cy2-1:cy2+1,cx2-1:cx2+1,1] = 0
-              
+            mouse = np.zeros_like(frame0)  
+            mouse = cv2.add(mouseLeft,mouseRight)
             frame_results = cv2.subtract(frame0,mouse) 
           
             cv2.imshow('frame0',frame_results)
@@ -235,10 +218,9 @@ class Window(tk.Frame):
             # Out put information such as frame number, area size etc
             ifr = round(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) 
             
-            mouse_size1 = np.count_nonzero(fgmask21)
-            mouse_size1 = np.count_nonzero(fgmask22)   
+            features = [videoName] + [ifr] + featuresLeft + featuresRight
                  
-            writer.writerow((videoName, ifr, cx1, cy1, mouse_size1, orientation1, cx1, cy1, mouse_size1, orientation1))
+            writer.writerow(features)
         
         cap.release()
         out.release()
